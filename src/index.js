bec00d3f6362425d1eb95251a402ea68e7b56334
Paul downing <pauldowning@Pauls-MacBook-Pro.local>
2020-07-08 12:30:52 -0600

    setup general app structure, testing, redux, and boostrap for later use

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import configureStore from './store/store';
+
+// components
+import App from './components/app';
+
+//styles
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/index.css';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store} >
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/store/reducers.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+  // to be used for importing reducers when features are built
+});
